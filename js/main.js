e1b50708000743d726081f6b01524a2654992348
Jesus Ruiz <rjesusdavid23@gmail.com>
2026-03-12 12:08:18 -0500

    feat: improve dino description formatting and fix import path in main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
-import dinos from "/data/dinos.js";
+import dinos from "../data/dinos.js";
 
 const container = document.getElementById("dino-container");
-function renderDinos(lista){
+
+function renderDinos(lista) {
+
     lista.forEach(dino => {
+
         const card = document.createElement("div");
-        card.classList.add("card");
+        card.classList.add("dino-card");
+
         card.innerHTML = `
             <img src="${dino.imagen}" alt="${dino.nombre}">
-            <h2>${dino.nombre}</h2>
-            <p><strong>Tipo:</strong> ${dino.tipo}</p>
-            <p><strong>Comida:</strong> ${dino.comida}</p>
-            <p><strong>Tameo:</strong> ${dino.tameo}</p>
-            <p><strong>Peligro:</strong> ${dino.peligro}</p>
+            
+            <div class="dino-info">
+                <h2>${dino.nombre}</h2>
+                <p><strong>Tipo:</strong> ${dino.tipo}</p>
+                <p><strong>Comida:</strong> ${dino.comida}</p>
+                <p><strong>Tameo:</strong> ${dino.tameo}</p>
+                <p><strong>Peligro:</strong> ${dino.peligro}</p>
+            </div>
         `;
+
         container.appendChild(card);
-    })
+
+    });
+
 }
+
 renderDinos(dinos);
